cf2730e990448c822efc47cace964b25a3469c56
cchadili <chadilisafi@gmail.com>
2025-12-17 18:28:52 +0100

    ex00

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,6 +1,9 @@
 #include "ScalarConverter.hpp"
 #include <stdlib.h>
 #include <limits.h>
+#include <iomanip> 
+#include <cmath>
+#include<float.h>
 
 ScalarConverter::ScalarConverter() {}
 ScalarConverter::ScalarConverter(const ScalarConverter &other) { (void)other; }
@@ -24,15 +27,11 @@ bool is_valid_input(std::string val, int &check)
             check = 5;
         return true;
     }
-
     bool has_dot = false;
     bool has_pos_sing = false;
     bool has_nig_sing = false;
     bool has_f = false;
     bool has_djt = false;
-
-    (void)has_f;
-
     for (size_t i = 0; i < val.size(); i++)
     {
         char c = val.at(i);
@@ -48,33 +47,21 @@ bool is_valid_input(std::string val, int &check)
         else if (c == 'f')
         {
             if (c != val.at(val.size() - 1))
-            {
-                check = -1;
                 return false;
-            }
             has_f = true;
         }
         else if (c == '.')
         {
             if (has_dot)
-            {
-                check = -1;
                 return false;
-            }
             has_dot = true;
         }
         else if(c == '+' || c == '-')
         {
             if (i != 0)
-            {
-                check=-1;
                 return false;
-            }
             if (has_nig_sing || has_pos_sing)
-            {
-                check = -1;
                 return false;
-            }
             if(c == '+')
                 has_pos_sing = true;
             else
@@ -84,16 +71,13 @@ bool is_valid_input(std::string val, int &check)
             return false;
         else
             has_djt = true;
-
     }
-
     if (has_dot && has_f && has_djt)
         check = 2;
     else if (has_dot && has_djt)
         check = 3;
     else if(has_djt)
         check = 1;
-    
     return true;
 }
 
@@ -101,9 +85,9 @@ void print_char(double value)
 {
     std::cout << "char: " ;
     char c = static_cast<char>(value);
-    if (value < 0 || value > 127)
+    if (value < 0 || value > 255)
         std::cout << "impossible";
-    else if (value < 32 || value == 127)
+    else if (!isprint(c))
         std::cout << "Non displayable";
     else
         std::cout << "'" << c << "'";
@@ -122,22 +106,35 @@ void print_int(double value)
     std::cout << std::endl;
 }
 
-void print_float(double value)
+void print_float(double value, int check)
 {
     std::cout << "float: ";
+    if (value < -FLT_MAX || value > FLT_MAX)
+    {
+        std::cout << "impossible";
+        return ;
+    }
     float f = static_cast<float>(value);
+    if(check == 1 || check == 0)
+        std::cout<< std::fixed  << std:: setprecision(1);
+    else
+        std::cout<< std::fixed  << std:: setprecision(6);
     std::cout << f ;
-    if (value == static_cast<int>(value))
-        std::cout << ".0";
     std::cout << "f" << std::endl;
 }
-void print_double(double value)
+void print_double(double value, int check)
 {
     std::cout << "double: ";
-
+    if (value == HUGE_VAL || value == -HUGE_VAL)
+    {
+        std::cout << "impossible";
+        return ;
+    }
+    if(check == 1 || check == 0)
+        std::cout<< std::fixed  << std:: setprecision(1) ;
+    else
+        std::cout<< std::fixed  << std:: setprecision(6);
     std::cout << value;
-    if(value == static_cast<int>(value))
-        std::cout << ".0";
     std::cout << std::endl;
 }
 
@@ -153,39 +150,32 @@ void ScalarConverter::convert(std::string value)
         return;
     }
     double holder = 0;
-    switch (check)
+    if(check == 0)
+        holder = static_cast<char>(value.at(0));
+    else if(check == 1 || check == 2 || check == 3)
     {
-        case (0) :
-            holder = static_cast<char>(value.at(0));
-            break;
-        case (1):
-            holder  = atof(value.c_str());
-            break;
-        case (2):
-            holder = atof(value.c_str());
-            break;
-        case (3):
-            holder = atof(value.c_str());
-            break;
-        case (4):
-            std::cout << "char: " << "impossible" << std::endl;
-            std::cout << "int: " << "impossible" << std::endl;
-            std::cout << "float: " << value << std::endl;
-            std::cout << "double: " << value.substr(0, value.size()-1) << std::endl;
-            return;
-        case (5):
-            std::cout << "char: " << "impossible" << std::endl;
-            std::cout << "int: " << "impossible" << std::endl;
-            std::cout << "float: " << value << 'f' << std::endl;
-            std::cout << "double: " << value << std::endl;
-            return;
-            break;
-        default:
-            break;
+        holder = atof(value.c_str());
     }
+    else if (check == 4)
+    {
+        std::cout << "char: " << "impossible" << std::endl;
+        std::cout << "int: " << "impossible" << std::endl;
+        std::cout << "float: " << value << std::endl;
+        std::cout << "double: " << value.substr(0, value.size()-1) << std::endl;
+        return ;
+    }
+    else if(check == 5)
+    {
+        std::cout << "char: " << "impossible" << std::endl;
+        std::cout << "int: " << "impossible" << std::endl;
+        std::cout << "float: " << value << 'f' << std::endl;
+        std::cout << "double: " << value << std::endl;
+        return ;
+    }
+    
     print_char(holder);
     print_int(holder);
-    print_float(holder);
-    print_double(holder);
+    print_float(holder, check);
+    print_double(holder, check);
 }
 
