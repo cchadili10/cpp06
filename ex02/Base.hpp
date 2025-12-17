c742786139c86869382e00f7519802ecbeac1427
cchadili <chadilisafi@gmail.com>
2025-12-17 20:32:17 +0100

    ex02

---
+++ ex02/Base.hpp
@@ -0,0 +1,7 @@
+class Base
+{
+    public:
+        Base();
+        virtual ~Base();
+};
+
---
+++ ex02/main.c
@@ -0,0 +1,11 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
